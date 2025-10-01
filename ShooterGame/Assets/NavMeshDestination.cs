52af1156ca6b7165f1b444a50d0c56b60b06365c
AnthonySaillant <2354779@csfoy.ca>
2025-10-01 09:17:20 -0400

    debut navmesh

---
+++ ShooterGame/Assets/NavMeshDestination.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Actor : MonoBehaviour
+{
+    [SerializeField] private GameObject goal;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if (navMeshAgent != null && goal != null)
+        {
+            navMeshAgent.SetDestination(goal.transform.position);
+        }
+    }
+
+    void Update()
+    {
+        if (goal == null) return;
+
+        if (navMeshAgent == null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, goal.transform.position, 5f * Time.deltaTime);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(goal.transform.position);
+        }
+    }
+}
