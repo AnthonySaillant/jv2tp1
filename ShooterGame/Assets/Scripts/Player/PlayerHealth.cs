64562901f225e984ef8a4f5f96ce1d83052281fc
MollySoja <lorikparent@gmail.com>
2025-10-08 09:29:25 -0400

    Bullets and playerHealth

--- ShooterGame/Assets/Scripts/Player/PlayerHealth.cs
+++ ShooterGame/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,69 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private int health = 5;
+    [SerializeField] private float invincibilityDuration = 0.5f;
+
+
+    private Coroutine invincibilityCoroutine;
+    private Rigidbody playerRigidBody;
+    private bool isGameOver = false;
+    private bool isInvincible = false;
+
+
     void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Alien"))
+        {
+            LoseHealth();
+        }
+    }
+
+    void LoseHealth()
+    {
+        if (isInvincible) return;
+
+        health -= 1;
+
+        if (invincibilityCoroutine != null)
+            StopCoroutine(invincibilityCoroutine);
+
+        invincibilityCoroutine = StartCoroutine(InvincibilityCoroutine());
+
+        if (health == 0)
+        {
+            GameOver();
+        }
+    }
+
+    private IEnumerator InvincibilityCoroutine()
+    {
+        isInvincible = true;
+
+        yield return new WaitForSeconds(invincibilityDuration);
+
+        isInvincible = false;
+    }
+
+    void GameOver()
+    {
+        isGameOver = !isGameOver;
+    }
+
+    public bool IsGameOver()
+    {
+        return isGameOver;
     }
 }
